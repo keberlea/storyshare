5f04decdebfb7d5d99fabe0c598f7913587529df
spamdalfz <coreyvasser@gmail.com>
2023-06-30 01:21:34 -0700

    styling touches for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,41 +15,43 @@ const Header = ({ activeTab, setActiveTab }) => {
 
     return (
         <header
-            className="px-6 py-4 bg-header-blue text-black text-4xl"
+            className="px-1 py-4 bg-header-blue text-black text-4xl"
         >
-
+            <div className="flex justify-center text-8xl mb-6 font-licorice text-inside text-black-stroke">
+                StoryShare
+            </div>
             <nav className="flex flex-wrap justify-around">
                 <NavLink
                     to="/home"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/home' ? 'underline font-bold' : ''} hover:bg-blue-500 hover:text-white`}
+                    className={`px-4 py-2 m-2 bg-button-yellow text-black font-marvel ${activeTab === '/home' ? 'underline font-bold' : ''} hover:bg-hover-pink hover:text-white`}
                 >
                     Home
                 </NavLink>
 
                 <NavLink
                     to="/signup"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/signup' ? 'underline font-bold' : ''} hover:bg-blue-500 hover:text-white`}
+                    className={`px-4 py-2 m-2 bg-button-yellow text-black font-marvel ${activeTab === '/signup' ? 'underline font-bold' : ''} hover:bg-hover-pink hover:text-white`}
                 >
                     Signup
                 </NavLink>
 
                 <NavLink
                     to="/login"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/login' ? 'underline font-bold' : ''} hover:bg-blue-500 hover:text-white`}
+                    className={`px-4 py-2 m-2 bg-button-yellow text-black font-marvel ${activeTab === '/login' ? 'underline font-bold' : ''} hover:bg-hover-pink hover:text-white`}
                 >
                     Log in
                 </NavLink>
 
                 <NavLink
                     to="/prompt"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/prompt' ? 'underline font-bold' : ''} hover:bg-blue-500 hover:text-white`}
+                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/prompt' ? 'underline font-bold' : ''} hover:bg-hover-pink hover:text-white`}
                 >
                     Prompts
                 </NavLink>
 
                 <NavLink
                     to="/stories"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/stories' ? 'underline font-bold' : ''} hover:bg-blue-500 hover:text-white`}
+                    className={`px-4 py-2 m-2 bg-button-yellow text-black ${activeTab === '/stories' ? 'underline font-bold' : ''} hover:bg-hover-pink hover:text-white`}
                 >
                     Stories
                 </NavLink>
@@ -58,6 +60,3 @@ const Header = ({ activeTab, setActiveTab }) => {
     );
 }
 export default Header;
-
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
+      fontFamily: {
+        'licorice': ["Licorice", 'cursive'],
+        'marvel': ["Marvel", 'sans-serif']
+      },
+      textColor: {
+        'inside': '#ffffff', // white color for text inside
+      },
+      textStroke: {
+        'black': '4px black',
+      },
       colors: {
         'header-blue': '#b8d1f6',
         'button-yellow': '#fee857',
         'app-color': '#72a1ed',
-        'foot-color': '#5e5d7c'
+        'foot-color': '#5e5d7c',
+        'hover-pink': '#5e5d7c'
       },
     },
   },

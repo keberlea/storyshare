ff335f696a7e6f70063af77999b937f0269fce4b
spamdalfz <coreyvasser@gmail.com>
2023-06-29 19:47:19 -0700

    modified app.js

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Prompt from "./components/Prompt";
 import Signup from "./components/Signup";
-import Stories from "./components/Stories"
+import Stories from "./components/Stories";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Helmet>
+        <title>StoryShare</title>
+      </Helmet>
+      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Routes>
+      </Routes>
+      <Footer />
+
+    </Router>
   );
 }
 

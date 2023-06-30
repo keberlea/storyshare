9a7e2165676960f9ab2eb1c600a37c36a7ea61d3
spamdalfz <coreyvasser@gmail.com>
2023-06-29 19:36:10 -0700

    added components and imported them into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Prompt from "./components/Prompt";
+import Signup from "./components/Signup";
+import Stories from "./components/Stories"
 
 function App() {
   return (

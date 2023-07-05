b323ec7e29ce1f1b82adea41a7feb71fffb11a2e
spamdalfz <coreyvasser@gmail.com>
2023-07-05 02:16:25 -0700

    more login modal styling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -33,19 +33,19 @@ const LoginModal = ({ onClose, onLoginSuccess }) => {
 
     return (
         <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-60" aria-hidden="true"></div>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-40" aria-hidden="true"></div>
             <AnimatedModal style={springProps} className="rounded-lg px-4 pt-5 pb-4 text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6 bg-app-color text-inside font-marvel">
-                <h3 className="text-2xl leading-6 font-bold text-inside text-stroke-black">
+                <h3 className="text-3xl leading-6 font-bold text-inside text-stroke-black">
                     Login
                 </h3>
                 <form onSubmit={handleSubmit} className="mt-4">
-                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-2" required />
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-4" required />
-                    <button type="submit" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-button-pink text-lg font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm">Login</button>
+                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-black text-xl border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-2" required />
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-black text-xl border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-4" required />
+                    <button type="submit" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-button-pink text-xl font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-lg">Login</button>
                 </form>
                 {/* Close the modal when the link is clicked */}
-                <p className="mt-4 text-sm">Don't have an account? <Link to="/signup" className="text-app-color hover:underline" onClick={onClose}>Sign up for StoryShare</Link></p>
-                <button type="button" className="mt-5 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-app-color text-lg font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm" onClick={onClose}>Close</button>
+                <p className="mt-4 text-lg">Don't have an account? <Link to="/signup" className="text-app-color hover:underline" onClick={onClose}>Sign up for StoryShare</Link></p>
+                <button type="button" className="mt-5 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-app-color text-xl font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-lg" onClick={onClose}>Close</button>
             </AnimatedModal>
         </div>
     );

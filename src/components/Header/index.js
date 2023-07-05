b5e71b8619e0e4dfeafa6eae462d1dc71add1f75
spamdalfz <coreyvasser@gmail.com>
2023-07-04 23:00:17 -0700

    login modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import LoginModal from '../Login';
 
 // create your styled component
+const StyledButton = styled.button`
+  margin-right: 15px;
+  margin-left: 15px;
+  transition: transform .2s;
+  box-shadow: 0px 4px 10px 3px rgba(0,0,0,0.75);
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
 const StyledNavLink = styled(NavLink)`
   margin-right: 15px;
   margin-left: 15px;
@@ -14,9 +25,19 @@ const StyledNavLink = styled(NavLink)`
 `;
 
 const Header = ({ activeTab, setActiveTab }) => {
+    const [showLogin, setShowLogin] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const location = useLocation();
 
+    // Assume we have a function that can check login status
+    const checkLoginStatus = async () => {
+        // Replace this with your actual function that checks the login status from the server
+        const loggedIn = Math.random() > 0.5; // This will randomly return true or false for now
+        setIsLoggedIn(loggedIn);
+    };
+
     useEffect(() => {
+        checkLoginStatus();
         const pathname = location.pathname.endsWith('/')
             ? location.pathname.slice(0, -1)
             : location.pathname;
@@ -29,7 +50,9 @@ const Header = ({ activeTab, setActiveTab }) => {
         <header
             className="px-6 py-4 bg-header-blue flex flex-col items-center justify-center"
         >
-            <h1 className="text-5xl text-white font-Licorice mb-6" id="title">StoryShare</h1>
+            <h1 className="text-5xl text-white font-Licorice mb-6" id="title">
+                StoryShare
+            </h1>
 
             <nav className="text-black text-4xl flex flex-wrap justify-around">
                 <StyledNavLink
@@ -46,12 +69,16 @@ const Header = ({ activeTab, setActiveTab }) => {
                     Signup
                 </StyledNavLink>
 
-                <StyledNavLink
-                    to="/login"
-                    className={`px-4 py-2 m-2 bg-button-yellow text-black font-marvel ${activeTab === '/login' ? 'underline' : ''} `}
-                >
-                    Log in
-                </StyledNavLink>
+                {!isLoggedIn && (
+                    <StyledButton
+                        onClick={() => setShowLogin(true)}
+                        className="px-4 py-2 m-2 bg-button-yellow text-black font-marvel"
+                    >
+                        Log in
+                    </StyledButton>
+                )}
+
+                {showLogin && <LoginModal onClose={() => setShowLogin(false)} />}
 
                 <StyledNavLink
                     to="/prompt"
@@ -70,4 +97,5 @@ const Header = ({ activeTab, setActiveTab }) => {
         </header>
     );
 }
+
 export default Header;
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
+const LoginModal = ({ onClose }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // call API to login the user
+        // await login({ username, password });
+
+        // If login is successful, redirect to home page
+        navigate("/home");
+    };
+
     return (
-        <div>
-            <h1>login page</h1>
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+
+                <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                    <div>
+                        <div className="mt-3 text-center sm:mt-5">
+                            <h3 className="text-lg leading-6 font-medium text-gray-900">
+                                Login
+                            </h3>
+                            <div>
+                                <form onSubmit={handleSubmit}>
+                                    <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="my-2 px-4 w-full h-12 border border-gray-300 rounded" required />
+                                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="my-2 px-4 w-full h-12 border border-gray-300 rounded" required />
+                                    <button type="submit" className="mt-4 bg-button-yellow text-black px-4 py-2 rounded font-marvel">Login</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-5 sm:mt-6">
+                        <button type="button" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-button-yellow text-base font-medium text-black hover:app-color focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm" onClick={onClose}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Login;
+export default LoginModal;

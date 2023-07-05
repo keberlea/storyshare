3ee5873a87ffb666488efcbe14432ae88fc30b1d
spamdalfz <coreyvasser@gmail.com>
2023-07-05 01:57:40 -0700

    modal styling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const AnimatedModal = styled(animated.div)`
 background-color: rgba(0, 0, 0, 0.8);
   padding: 3em;
-  width: 720px;
+  height: 550px;
   border-radius: 10px;
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
 `;
@@ -33,7 +33,7 @@ const LoginModal = ({ onClose, onLoginSuccess }) => {
 
     return (
         <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75" aria-hidden="true"></div>
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-60" aria-hidden="true"></div>
             <AnimatedModal style={springProps} className="rounded-lg px-4 pt-5 pb-4 text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6 bg-app-color text-inside font-marvel">
                 <h3 className="text-2xl leading-6 font-bold text-inside text-stroke-black">
                     Login
@@ -41,11 +41,11 @@ const LoginModal = ({ onClose, onLoginSuccess }) => {
                 <form onSubmit={handleSubmit} className="mt-4">
                     <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-2" required />
                     <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full px-3 py-2 placeholder-gray-500 text-sm border rounded-md focus:outline-none focus:ring-2 focus:ring-button-pink mb-4" required />
-                    <button type="submit" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-button-pink text-lg font-medium text-inside hover:bg-button-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm">Login</button>
+                    <button type="submit" className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-button-pink text-lg font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm">Login</button>
                 </form>
                 {/* Close the modal when the link is clicked */}
-                <p className="mt-4 text-sm">Don't have an account? <Link to="/signup" className="text-app-color hover:underline" onClick={onClose}>Sign up for YourApp</Link></p>
-                <button type="button" className="mt-5 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-app-color text-lg font-medium text-inside hover:bg-button-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm" onClick={onClose}>Close</button>
+                <p className="mt-4 text-sm">Don't have an account? <Link to="/signup" className="text-app-color hover:underline" onClick={onClose}>Sign up for StoryShare</Link></p>
+                <button type="button" className="mt-5 inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-app-color text-lg font-medium text-inside hover:bg-button-yellow hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-button-yellow sm:text-sm" onClick={onClose}>Close</button>
             </AnimatedModal>
         </div>
     );

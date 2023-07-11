f21f06f73a16df23feceb4af591ca3c1dcf5768b
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-07-10 19:57:01 -0700

    added to client/src/utils

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,97 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!) {
+  addUser(username: $username, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+`;
+
+export const ADD_STORY = gql`
+mutation addStory($title: String!, $prompt: String!, $storyText: String!) {
+    addStory(title: $title, prompt: $prompt, storyText: $storyText) {
+        _id
+        title
+        prompt
+        storyText
+        createdAt
+        username
+        comments {
+            _id
+            commentText
+            createdAt
+            username
+        }
+    }
+}
+`;
+
+export const ADD_COMMENT = gql`
+mutation addComment($storyId: ID!, $commentText: String!) {
+    addComment(storyId: $storyId, commentText: $commentText) {
+        _id
+        commentCount
+        comments {
+            _id
+            commentText
+            createdAt
+            username
+        }
+    }
+}
+`;
+
+export const UPDATE_STORY = gql`
+mutation updateStory($storyId: ID!, $title: String!, $prompt: String!, $storyText: String!) {
+    updateStory(storyId: $storyId, title: $title, prompt: $prompt, storyText: $storyText) {
+        _id
+        title
+        prompt
+        storyText
+        createdAt
+        username
+        comments {
+            _id
+            commentText
+            createdAt
+            username
+        }
+    }
+}
+`;
+
+export const DELETE_STORY = gql`
+mutation deleteStory($storyId: ID!) {
+    deleteStory(storyId: $storyId) {
+        _id
+        title
+        prompt
+        storyText
+        createdAt
+        username
+        comments {
+            _id
+            commentText
+            createdAt
+            username
+        }
+    }
+}
+`;
+
+

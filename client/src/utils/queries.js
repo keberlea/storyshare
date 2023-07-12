7d6848c4179c8962281ab916032d38c446bce47b
Alicia Keberle <122774607+keberlea@users.noreply.github.com>
2023-07-12 13:17:41 -0700

    createstory posts to database

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
+        _id
         username
         stories {
             title
